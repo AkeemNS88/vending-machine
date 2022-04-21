45a7da12e89ac3d7479c983fdb18ebe1f8fb6bbf
Akeem <AkeemS11788@gmail.com>
2022-04-21 16:29:22 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Button from "./components/Button";
+import Display from "./components/Display";
 
 function App() {
+  const [input, setInput] = useState('');
+  const [currentNumber, setCurrentNumber] = useState('0');
+  const [operator, setOperator] = useState(undefined);
+  const [equalPressed, setEqualPressed] = useState(false);
+
+  const buttons = [
+    { label: 'AC', id: 'clear', class: 'reset'},
+    { label: 'A', id: 'letter-A', class: 'letter'},
+    { label: 'B', id: 'letter-B', class: 'letter'},
+    { label: 7, id: 'seven', class: 'number'},
+    { label: 8, id: 'eight', class: 'number'},
+    { label: 9, id: 'nine', class: 'number'},
+    { label: 'C', id: 'letter-C', class: 'letter'},
+    { label: 4, id: 'four', class: 'number'},
+    { label: 5, id: 'five', class: 'number'},
+    { label: 6, id: 'six', class: 'number'},
+    { label: '-', id: 'subtract', class: 'operator'},
+    { label: 1, id: 'one', class: 'number'},
+    { label: 2, id: 'two', class: 'number'},
+    { label: 3, id: 'three', class: 'number'},
+    { label: '+', id: 'add', class: 'operator'},
+    { label: 0, id: 'zero', class: 'zero'},
+    { label: '.', id: 'decimal', class: 'decimal'},
+    { label: '=', id: 'equals', class: 'equals'}
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="vending-machine">
+       <Display input={input} currentNumber={currentNumber} operator={operator} />
+       <div className="keyboard">
+        <div className="button-container">
+          {buttons.map(btn => (
+              <Button key={btn.id} btn={btn} input={input} setInput={setInput} currentNumber={currentNumber} setCurrentNumber={setCurrentNumber} operator={operator} setOperator={setOperator} equalPressed={equalPressed} setEqualPressed={setEqualPressed} />
+            )
+          )}
+        </div>
+        <div className="speaker"></div>       
+       </div>
+     </div>
     </div>
   );
 }
